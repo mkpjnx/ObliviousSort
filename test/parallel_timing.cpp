80df5a530462a3cb1788d6c7d5c768e116d310cd
mkpjnx <mikepjxu@gmail.com>
2021-03-16 12:22:51 -0400

    Parallel timing code with args

--- test/parallel_timing.cpp
+++ test/parallel_timing.cpp
@@ -5,23 +5,38 @@
 #include <climits>
 #include <omp.h>
 #include <chrono>
+#include <iostream>
 #include "src/include/storage/vectorStore.h"
 #include "src/include/osort/recorba.h"
 #include "src/include/osort/quicksort.h"
 #include "src/include/util/common.h"
 
+bool testNum = false;
+bool testThr = false;
+
+size_t ttestN = 10000;
+size_t ttestTrials = 3;
+
 size_t ntestScale = 2;
 size_t ntestMin = 1000;
-size_t ntestMax = 10000000;
-size_t numTrials = 3;
+size_t ntestMax = 100000;
+size_t ntestTrials = 3;
 
-double timeOrba(size_t N, size_t numThreads, size_t bucketSize, size_t gamma){
+size_t bucketSize = 512;
+
+//Returns the time taken to run Rec-ORBA using the following specifications
+double timeOrba(size_t N, size_t numThreads, size_t bucketSize){
   auto elems = libStorage::VectorElementStore<int>(N);
-  size_t numBuckets = N / (bucketSize / 2) + 1;
-  numBuckets = 1UL << (size_t)(std::ceil(std::log2(numBuckets)));
+  size_t paddedN = 1UL << (size_t)(std::ceil(std::log2(N)));
+
+  size_t numBuckets = paddedN / (bucketSize / 2);
+  numBuckets = numBuckets ? numBuckets : 1;
   auto storage = libStorage::VectorBucketStore<libUtil::Labeled<int>>(numBuckets, bucketSize);
 
-  omp_set_num_threads(omp_get_max_threads());
+  // set gamma = log_2(n), padded to pow2
+  size_t gamma = (size_t)(std::ceil(std::log2(N)));
+  gamma =  1UL << (size_t)(std::ceil(std::log2(gamma)));
+  omp_set_num_threads(omp_get_num_procs());
   #pragma omp parallel for shared(elems) schedule(static)
   for (size_t eid = 0; eid < elems.Size; eid++) {
     int num = eid;
@@ -46,26 +61,39 @@ double timeOrba(size_t N, size_t numThreads, size_t bucketSize, size_t gamma){
   return elapsed_seconds.count();
 }
 
-void numScaling(size_t numThreads, size_t bucketSize, size_t gamma){
-  printf("N, time(s)\n");
+void numScaling(){
+  size_t numThreads = (size_t)omp_get_num_procs();
+  std::cout << "**********************\n"
+            << "**  N Scaling test  **\n"
+            << "**********************\n"
+            << "T = " << numThreads << "\n"
+            << "Running " << ntestTrials << " trials each\n\n" 
+            << "N, time(s)\n";
   for(size_t N = ntestMin; N < ntestMax; N*=2){
     double t = 0;
-    for(size_t trial = 0; trial < numTrials; trial++){
-      t += timeOrba(N, numThreads, bucketSize, 4);
+    for(size_t trial = 0; trial < ntestTrials; trial++){
+      t += timeOrba(N, numThreads, bucketSize);
     }
-    printf("N= %lu, t = %f\n", N, t / numTrials);
+    std::cout << N << "," <<  t / ntestTrials << "\n";
   }
+  std::cout << "\nDone!\n";
 }
 
-void thrScaling(size_t N, size_t bucketSize, size_t gamma){
-  printf("#threads, time(s)\n");
-  for(size_t numThreads = 1; numThreads <= omp_get_max_threads(); numThreads++){
+void thrScaling(){
+  std::cout << "**********************\n"
+            << "** Thr Scaling test **\n"
+            << "**********************\n"
+            << "N = " << ttestN << "\n"
+            << "Running " << ttestTrials << " trials each\n\n" 
+            << "#threads, time(s)\n";
+  for(int numThreads = 1; numThreads <= omp_get_num_procs(); numThreads++){
     double t = 0;
-    for(size_t trial = 0; trial < numTrials; trial++){
-      t += timeOrba(N, numThreads, bucketSize, 4);
+    for(size_t trial = 0; trial < ttestTrials; trial++){
+      t += timeOrba(ttestN, (size_t) numThreads, bucketSize);
     }
-    printf("%lu, %f\n", N, t / numTrials);
+    std::cout << numThreads << "," <<  t / ttestTrials << "\n";
   }
+  std::cout << "\nDone!\n";
 }
 
 
@@ -82,9 +110,37 @@ void randomElems(libStorage::VectorElementStore<int> &elems){
   }
 }
 
+void printUsage(char **argv){
+  std::cout << "Usage: " << argv[0]
+            << " [-n min max trials] [-t n trials] [-b bucket size]\n";
+  std::cout << "\t-n\tTest scaling with respect to array size. Doubling each time\n"
+            << "\t-t\tTest scaling with respect to number of threads\n";
+  exit(0);
+}
 
 int main(int argc, char **argv) {
-  numScaling(8, 512, 4);
-  thrScaling(1000000, 512, 4);
+  if (argc == 1) printUsage(argv);
+  for (int i = 1; i < argc; i++){
+    if (std::string(argv[i]) == "-n") {
+      testNum = true;
+      if(i+3 >= argc) printUsage(argv);
+
+      ntestMin = atoi(argv[++i]);
+      ntestMax = atoi(argv[++i]);
+      ntestTrials = atoi(argv[++i]);
+      if(!ntestMin || ntestMax < ntestMin || !ntestTrials) printUsage(argv);
+    } else if (std::string(argv[i]) == "-t") {
+      testThr = true;
+      if(i+2 >= argc) printUsage(argv);
+      ttestN = atoi(argv[++i]);
+      ttestTrials = atoi(argv[++i]);
+      if(!ttestTrials || !ttestN) printUsage(argv);
+    } else {
+      printUsage(argv);
+    }
+  }
+
+  if (testNum) numScaling();
+  if (testThr) thrScaling();
   return 0;
 }
