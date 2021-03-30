233131b87a61d58fa5b0ba405bf1fdc9cad9652a
mkpjnx <mikepjxu@gmail.com>
2021-03-29 20:32:16 -0400

    more baseline comparisons

--- test/parallel_baseline.cpp
+++ test/parallel_baseline.cpp
@@ -16,9 +16,72 @@ void worker(std::vector<int> &vec, size_t itr, size_t offset, size_t N) {
   }
 }
 
+void parRec(std::vector<int> &vec, size_t itr, size_t offset, size_t N, size_t threshold) {
+  if (N <= threshold) {
+    worker(vec, itr, offset, N);
+    return;
+  }
+  #pragma omp parallel num_threads(2)
+  {
+    int num = omp_get_thread_num();
+    parRec(vec, itr, offset + num * N / 2, N/2, threshold);
+    #pragma omp barrier
+  }
+}
+
+void taskRec(std::vector<int> &vec, size_t itr, size_t offset, size_t N, size_t threshold) {
+  if (N <= threshold) {
+    worker(vec, itr, offset, N);
+    return;
+  }
+  #pragma omp task
+    taskRec(vec, itr, offset, N/2, threshold);
+  #pragma omp task
+    taskRec(vec, itr, offset + N / 2, N/2, threshold);
+  #pragma omp taskwait
+}
+
+double timeParrec(size_t itr, size_t total, size_t numThreads, size_t task_size){
+  std::vector<int> vec;
+  vec.resize(total);
+  omp_set_nested(omp_get_max_active_levels());
+  #pragma omp parallel for
+  for (size_t i = 0; i < total; i++) {
+    vec[i] = (int)i;
+  }
+
+  auto parastart = std::chrono::steady_clock::now();
+  
+  parRec(vec, itr, 0, total, task_size);
+  
+  auto paraend = std::chrono::steady_clock::now();
+  std::chrono::duration<double> elapsed_seconds = paraend-parastart;
+  return elapsed_seconds.count();
+}
+
+
+double timeTaskrec(size_t itr, size_t total, size_t numThreads, size_t task_size){
+  std::vector<int> vec;
+  vec.resize(total);
+  omp_set_num_threads(numThreads);
+  #pragma omp parallel for
+  for (size_t i = 0; i < total; i++) {
+    vec[i] = (int)i;
+  }
+
+  auto parastart = std::chrono::steady_clock::now();
+  #pragma omp parallel
+  {
+    #pragma omp single
+    taskRec(vec, itr, 0, total, task_size);
+  }
+  auto paraend = std::chrono::steady_clock::now();
+  std::chrono::duration<double> elapsed_seconds = paraend-parastart;
+  return elapsed_seconds.count();
+}
 
 //Returns the time taken to run Rec-ORBA using the following specifications
-double timeWorkers(size_t itr, size_t total, size_t numThreads, size_t task_size){
+double timeParfor(size_t itr, size_t total, size_t numThreads, size_t task_size){
   std::vector<int> vec;
   vec.resize(total);
 
@@ -30,8 +93,8 @@ double timeWorkers(size_t itr, size_t total, size_t numThreads, size_t task_size
   omp_set_num_threads(numThreads);
   auto parastart = std::chrono::steady_clock::now();
   #pragma omp parallel for
-  for (size_t i = 0; i < total - task_size; i+= task_size) {
-    worker(vec, itr, i, task_size);
+  for (size_t i = 0; i < total/task_size; i++) {
+    worker(vec, itr, i * task_size, task_size);
   }
   
   auto paraend = std::chrono::steady_clock::now();
@@ -39,6 +102,33 @@ double timeWorkers(size_t itr, size_t total, size_t numThreads, size_t task_size
   return elapsed_seconds.count();
 }
 
+//Returns the time taken to run Rec-ORBA using the following specifications
+double timeTaskloop(size_t itr, size_t total, size_t numThreads, size_t task_size) {
+  std::vector<int> vec;
+  vec.resize(total);
+
+  #pragma omp parallel for
+  for (size_t i = 0; i < total; i++) {
+    vec[i] = (int)i;
+  }
+
+  omp_set_num_threads(numThreads);
+  auto parastart = std::chrono::steady_clock::now();
+  #pragma omp parallel
+  {
+    #pragma omp single
+    {
+      #pragma omp taskloop
+      for (size_t i = 0; i < total/task_size; i++) {
+        worker(vec, itr, i, task_size);
+      }
+    }
+  }
+  
+  auto paraend = std::chrono::steady_clock::now();
+  std::chrono::duration<double> elapsed_seconds = paraend-parastart;
+  return elapsed_seconds.count();
+}
 
 double timeSerial(size_t itr, size_t total){
   std::vector<int> vec;
@@ -57,18 +147,22 @@ double timeSerial(size_t itr, size_t total){
 
 void problemScaling(){
   size_t minN = 1UL << 10;
-  size_t maxN = 1UL << 20;
+  size_t maxN = 1UL << 15;
   size_t itr = 100;
 
   int procs = omp_get_num_procs();
-  std::cout << "N,serial,single,parallel\n";
+  std::cout << "N,serial,singleParfor,multiParfor,singleTask,multiTask\n";
   for (size_t N = minN; N <= maxN; N *= 2) {
     double serial = timeSerial(itr, N);
-    double single = timeWorkers(itr, N, 1, N);
-    double parallel = timeWorkers(itr, N, procs, N / procs);
-    std::cout << N << "," << serial << "," << single << "," << parallel << "\n";
+    double singleParFor = timeParfor(itr, N, 1, N);
+    double multiParfor = timeParfor(itr, N, procs, N / procs);
+    double singleTask = timeTaskloop(itr, N, 1, N);
+    double multiTask = timeTaskloop(itr, N, procs, N / procs);
+    std::cout << N << "," << serial << ","
+              << singleParFor << "," << multiParfor << ","
+              << singleTask << "," << multiTask << "\n";
   }
-
+  std::cout << "\n";
 
 }
 
@@ -79,17 +173,37 @@ void granScaling(){
   size_t max_task_size = N/procs;
   size_t itr = 10;
 
-  std::cout << "taskSize,serial,single,parallel\n";
+  std::cout << "taskSize,serial,parfor,taskloop\n";
   for (size_t tsize = min_task_size; tsize <= max_task_size; tsize *= 2) {
     double serial = timeSerial(itr, N);
-    double parallel = timeWorkers(itr, N, procs, tsize);
-    std::cout << tsize << "," << serial << "," << parallel << "\n";
+    double parfor = timeParfor(itr, N, procs, tsize);
+    double taskloop = timeTaskloop(itr, N, procs, tsize);
+    std::cout << tsize << "," << serial << "," << parfor << "," << taskloop << "\n";
   }
+  std::cout << "\n";
 
+}
+
+void recScaling(){
+  size_t N = 1UL << 20;
+  size_t min_task_size = 1UL << 6;
+  int procs = omp_get_num_procs();
+  size_t max_task_size = N/procs;
+  size_t itr = 10;
+
+  std::cout << "taskSize,serial,parfor,taskloop\n";
+  for (size_t tsize = min_task_size; tsize <= max_task_size; tsize *= 2) {
+    double serial = timeSerial(itr, N);
+    double parrec = timeParrec(itr, N, procs, tsize);
+    double taskrec = timeTaskrec(itr, N, procs, tsize);
+    std::cout << tsize << "," << serial << "," << parrec << "," << taskrec << "\n";
+  }
+  std::cout << "\n";
 }
 
 int main(int argc, char **argv) {
   problemScaling();
   granScaling();
+  recScaling();
   return 0;
 }
